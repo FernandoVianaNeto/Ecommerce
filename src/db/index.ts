ccc72fb0e3f2b986809c890656f02cf98b5cd7b6
Fernando Viana <fernando.viana.nt@gmail.com>
2025-08-19 17:26:12 -0300

    chore: fix type error from db index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,6 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 import * as schema from "./schema";
 
-export const db = drizzle(process.env.DATABASE_URL, {
+export const db = drizzle(process.env.DATABASE_URL as string, {
     schema,
 });
