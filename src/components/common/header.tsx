04f18a440c4c92460223919f09535d5c0a92ec24
Fernando Viana <fernando.viana.nt@gmail.com>
2025-08-20 20:43:19 -0300

    refactor: product-variant responsive

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -7,7 +7,7 @@ import HeaderMenu from "./header-menu";
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between p-5">
+        <header className="flex items-center justify-between py-5 px-4 sm:px-6 lg:px-8">
             <Link href="/">
                 <Image src="/logo.svg" alt="bewear logo" className="h-8 w-auto" width={100} height={26.14}/>
             </Link>
